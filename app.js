85c41175606b2db08ffb8c136fc846c6617f2211
PavankumarAN <pavankumaran51@gmail.com>
2019-04-28 01:55:48 +0530

    Procfile

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 const http = require('http');
+const express = require('express');
+const app = express();
+var port = process.env.PORT || 8080;
 
-let server = http.createServer((req, res) => {
-    // res.writeHead(200, { 'content-type': 'text/plain' });
-    console.log("Hai");
-    res.end("Hello World");
+// let server = http.createServer((req, res) => {
+//     // res.writeHead(200, { 'content-type': 'text/plain' });
+//     console.log("Hai");
+//     res.end("Hello World");
+// });
+
+app.get('/',(req,res) =>{
+    res.sendFile(__dirname+'/index.html');
 });
 
-server.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server is running at port number 3000");
 
 });
